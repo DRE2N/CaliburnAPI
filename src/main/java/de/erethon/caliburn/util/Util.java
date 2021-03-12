c9e586a3dfd9d2f8de9c5a79f6ec575fcb27ec81
Fyreum <luka.vogel@gmx.de>
2021-03-12 12:47:12 +0100

    Removed unneeded method Util#getChar()

--- src/main/java/de/erethon/caliburn/util/Util.java
+++ src/main/java/de/erethon/caliburn/util/Util.java
@@ -57,35 +57,4 @@ public class Util {
         }
         return parsed;
     }
-
-    public static char getChar(int i) {
-        if (i < 0 | i > 9) {
-            throw new ValidationException("Chars can only be integers between 0-9");
-        }
-        switch (i) {
-            case 0:
-                return '0';
-            case 1:
-                return '1';
-            case 2:
-                return '2';
-            case 3:
-                return '3';
-            case 4:
-                return '4';
-            case 5:
-                return '5';
-            case 6:
-                return '6';
-            case 7:
-                return '7';
-            case 8:
-                return '8';
-            case 9:
-                return '9';
-        }
-        // shouldn't happen
-        return ' ';
-    }
-
 }
