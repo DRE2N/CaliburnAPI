b57dcbd21bf1678f9f29da5253140c6ac1a3d740
Daniel Saukel <Sataniel98@users.noreply.github.com>
2022-04-03 17:28:32 +0200

    Bump year

--- api/src/main/java/de/erethon/caliburn/util/SimpleSerialization.java
+++ api/src/main/java/de/erethon/caliburn/util/SimpleSerialization.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2021 Daniel Saukel.
+ * Copyright (C) 2015-2022 Daniel Saukel.
  *
  * This library is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
