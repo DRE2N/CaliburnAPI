ab4a763a018ed35132ce0e86c53b61e5e8e4e76e
Daniel Saukel <Sataniel98@users.noreply.github.com>
2022-04-04 16:56:11 +0200

    Fix batch

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-mvn clean install -D"maven.wagon.http.ssl.insecure=true" -D"maven.wagon.http.ssl.allowall=true" -D"maven.wagon.http.ssl.ignore.validity.dates=true"
+mvn clean install -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
